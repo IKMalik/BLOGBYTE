9987cb7d4604dd7862ca7d0a1662820ccdc811e3
Ibrahim <{32967336}+{IKMalik}@users.noreply.github.com>
2021-06-30 23:54:47 +0100

    Added gitignore and page redirects

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def homepage(request):
+    return HttpResponse("<h1> Homepage </h1>")
+
+def siteInformation(request):
+    return HttpResponse("<h1> This is the information about my blog </h1>")
