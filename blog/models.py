227dde88655adcfc52dfae2921d4733d6419f2f7
Ibrahim <{32967336}+{IKMalik}@users.noreply.github.com>
2021-07-01 18:25:33 +0100

    Added functionality to post new blog which are visible on the home page, all prior blogs are also on the homepage with everything stored in mongoDb database

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 from djongo import models
+from django.urls import reverse
 
 # Create your models here.
 
 class Posts(models.Model):
     id = models.ObjectIdField()
-    author = models.JSONField()
+    author = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    date_posted = models.DateTimeField(auto_now_add=False)
+    date_posted = models.DateTimeField(auto_now_add=True)
     objects = models.DjongoManager()
+
     
